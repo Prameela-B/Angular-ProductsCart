f766ab5eb1dc1c961beff5cb3aa694d49fa651fc
Prameela-B <bprame@gmail.com>
2020-04-24 13:41:15 -0400

    1 file added, 1 deleted

---
+++ src/app/employee.ts
@@ -0,0 +1,30 @@
+export const employee = [
+  {
+    firstname: 'John',
+    lastname:'Paul',
+    empid: 799,
+    dob:'04/21/1980',
+    designation: 'Manager'
+  },
+  {
+    firstname: 'Rick',
+    lastname:'Tom',
+    empid: 699,
+    dob:'01/11/1991',
+    designation: 'Accountant'
+  },
+  {
+    firstname: 'Dave',
+    lastname:'Cal',
+    empid: 399,
+    dob:'05/31/1999',
+    designation: 'SalesPerson'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
